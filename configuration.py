c00a2731c572c88f560f4035673a91d37ad32715
Ratzerra <ratzerra@yandex.ru>
2023-07-24 21:42:14 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://92e865bf-14a5-4781-9c56-61b016e70254.serverhub.praktikum-services.ru/"
+CREATE_ORDER = "api/v1/orders"
+REQUEST_ORDER = "api/v1/orders/track"
---
+++ create_order_test.py
@@ -0,0 +1,18 @@
+import data
+import sender_test
+
+def create_order():
+    current_body = data.order_content.copy()
+    current_body ["firstName"] = "Ivan"
+    track_num = sender_test.post_new_order(current_body)
+    return str(track_num.json()["track"])
+
+def positive_assert():
+    track_num = create_order()
+    current_params = data.params_get.copy()
+    current_params["t"] = track_num
+    response = sender_test.get_order(current_params)
+    assert response.status_code == 200
+
+def test_order():
+    positive_assert()
